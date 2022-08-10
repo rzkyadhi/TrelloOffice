ed4e39f8effd7062104a112e919c480aead9bec1
aziz23598 <azizkuncor26@gmail.com>
2022-08-10 13:31:46 +0700

    repository - Aziz

---
+++ API/API/Repositories/Data/ProjectRepository.cs
@@ -0,0 +1,12 @@
+﻿using API.Models;
+
+namespace API.Repositories.Data
+{
+    public class ProjectRepository : GenericRepository<Project>
+    {
+        public ProjectRepository(string request = "Project", string joinRequest="None") : base(request, joinRequest)
+        {
+
+        }
+    }
+}
--- API/API/Repositories/Interface/IGeneralRepository.cs
+++ API/API/Repositories/Interface/IGeneralRepository.cs
@@ -2,11 +2,11 @@
 
 namespace API.Repository.Interface
 {
-    public interface IGeneralRepository<TModel, TPrimaryKey>
+    public interface IGeneralRepository<TModel>
         where TModel : class
     {
         List<TModel> Get();
-        TModel Get(TPrimaryKey Id);
+        TModel Get(int Id);
         int Post(TModel model);
         int Put(TModel model);
         int Delete(TModel model);
