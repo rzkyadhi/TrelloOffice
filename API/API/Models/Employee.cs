d0479da1df32bb96d070b3d8b077c05a932ebacb
rzkyadhi <rizkyadhi081@gmail.com>
2022-08-10 09:37:09 +0700

    Tambah Models Folder

---
+++ API/API/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        public string BirthDate { get; set; }
+        public string Gender { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        public string HireDate { get; set; }
+    }
+}
