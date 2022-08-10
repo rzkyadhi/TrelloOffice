798164ab17dff035345e089b29a91f838967ebf8
aziz23598 <azizkuncor26@gmail.com>
2022-08-10 14:37:02 +0700

    Controller - Aziz

---
+++ API/API/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using API.Base;
+using API.Models;
+using API.Repositories.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : BaseController<Category, CategoryRepository>
+    {
+        CategoryRepository repository;
+
+        public CategoryController(CategoryRepository repository) : base(repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
