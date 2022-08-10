17f19b0ebf44edd8fc8294ea0d29c68c22773df1
rzkyadhi <rizkyadhi081@gmail.com>
2022-08-10 14:09:20 +0700

    Fix Migrasi Gender DataType

---
+++ API/API/Migrations/20220810070743_ChangeGenderDataType.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class ChangeGenderDataType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "TB_M_EMPLOYEE",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Gender",
+                table: "TB_M_EMPLOYEE",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
--- API/API/Models/Employee.cs
+++ API/API/Models/Employee.cs
@@ -12,14 +12,9 @@ namespace API.Models
         [DataType(DataType.DateTime)]
         public string BirthDate { get; set; }
         [Required]
-        public Gender Gender { get; set; }
+        public string Gender { get; set; }
         [Required]
         [DataType(DataType.DateTime)]
         public string HireDate { get; set; }
-    }
-    public enum Gender
-    {
-        Male,
-        Female
     }
 }
