cc13f3aec49169a772e93f7c572e31a100496a12
rzkyadhi <rizkyadhi081@gmail.com>
2022-08-10 14:30:41 +0700

    Tambah TaskController - KIKIGANTENG

---
+++ API/API/Controllers/TaskController.cs
@@ -0,0 +1,38 @@
+﻿using API.Base;
+using API.Models;
+using API.Repositories.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : BaseController<Task, TaskRepository>
+    {
+        private readonly TaskRepository repository;
+
+        public TaskController(TaskRepository repository) : base(repository)
+        {
+            this.repository = repository;
+        }
+
+        public ActionResult<List<Task>> Get()
+        {
+            var result = repository.Get();
+            if (result != null)
+                return Ok(new
+                {
+                    status = 200,
+                    message = "SUCCESS",
+                    data = result
+                });
+            return NotFound(new
+            {
+                status = 404,
+                message = "NOT FOUND"
+            });
+        }
+    }
+}
