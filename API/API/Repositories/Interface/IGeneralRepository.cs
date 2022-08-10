fc5b25cc67700f8b0d67108a53756801f759acaa
aziz23598 <azizkuncor26@gmail.com>
2022-08-10 11:02:42 +0700

    Add Interface hari 1 - Aziz

---
+++ API/API/Repositories/Interface/IGeneralRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace API.Repository.Interface
+{
+    public interface IGeneralRepository<TModel, TPrimaryKey>
+        where TModel : class
+    {
+        List<TModel> Get();
+        TModel Get(TPrimaryKey Id);
+        int Post(TModel model);
+        int Put(TModel model);
+        int Delete(TModel model);
+    }
+}
