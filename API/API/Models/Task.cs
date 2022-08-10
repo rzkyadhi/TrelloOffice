21f27ed9cc575a06b56dcb25a6d944030acddac2
aziz23598 <azizkuncor26@gmail.com>
2022-08-10 10:04:43 +0700

    Add Models Hari 1 - AZIZGANTENG

---
+++ API/API/Models/Task.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+    public class Task
+    {
+        [Key]
+        public int TaskId { get; set; }
+
+        [Required]
+        [ForeignKey("RoleUserTask")]
+        public int RoleUserTaskId { get; set; }
+        public RoleUserTask RoleUserTask { get; set; }
+
+        [Required]
+        [ForeignKey("Project")]
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+
+        [Required]
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public string DueDate { get; set; }
+
+        [Required]
+        public bool IsCompleted { get; set; }
+    }
+}
