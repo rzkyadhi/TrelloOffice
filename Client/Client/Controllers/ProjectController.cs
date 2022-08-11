0b1247d6f4a3bd3d6ea1db8c68731c53bee96299
aziz23598 <azizkuncor26@gmail.com>
2022-08-11 10:53:42 +0700

    Client = model,repo,controller - Aziz

---
+++ Client/Client/Controllers/ProjectController.cs
@@ -0,0 +1,86 @@
+﻿using Client.Models;
+using Client.Repositories.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly ProjectRepository projectRepository;
+
+        public ProjectController(ProjectRepository projectRepository)
+        {
+            this.projectRepository = projectRepository;
+        }
+
+        #region Get
+        public IActionResult Index()
+        {
+            var result = projectRepository.Get();
+            if (result != null)
+                return View(result);
+            return View();
+        }
+        #endregion Get
+
+        #region Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Project project)
+        {
+            var result = projectRepository.Post(project);
+            if (result > 0)
+                return RedirectToAction("Index", "Project");
+            return View();
+        }
+        #endregion Create
+
+        #region Edit
+        public IActionResult Edit(int id)
+        {
+            var result = projectRepository.Get(id);
+            if (result != null)
+                return View(result);
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = projectRepository.Put(project);
+                if (result > 0)
+                    return RedirectToAction("Index", "Project");
+            }
+            return View();
+        }
+        #endregion Edit
+
+        #region Delete
+        public IActionResult Delete(int id)
+        {
+            var result = projectRepository.Get(id);
+            if (result != null)
+                return View(result);
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Project project)
+        {
+            var result = projectRepository.Delete(project);
+            if (result > 0)
+                return RedirectToAction("Index", "Project");
+            return View();
+        }
+        #endregion Delete
+    }
+}
---
+++ Client/Client/Repositories/Data/ProjectRepository.cs
@@ -0,0 +1,12 @@
+﻿using Client.Models;
+
+namespace Client.Repositories.Data
+{
+    public class ProjectRepository : GenericRepository<Project>
+    {
+        public ProjectRepository(string request = "Project") : base(request)
+        {
+
+        }
+    }
+}
